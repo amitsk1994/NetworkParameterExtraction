cc77911e33a8c085027ca572b9e32126f376aaa9
Amit <amit@Amits-MacBook-Pro.local>
2019-09-10 19:54:25 -0400

    first-commit

---
+++ graph.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as pyp
+import matplotlib.animation as animation
+
+figure=pyp.figure()
+subplot= figure.add_subplot(1,1,1)
+
+def animation_function(i):
+    cpu_data=open("/Users/amit/Documents/NetworkParameterExtraction/cpu.txt").readlines()
+
+    x=[]
+
+    for each_value in cpu_data:
+        if(len(each_value))>1:
+            x.append(float(each_value))
+
+    subplot.clear()
+    subplot.plot(x)
+
+graph_animation=animation.FuncAnimation(figure,animation_function,interval=100000)
+pyp.show()
